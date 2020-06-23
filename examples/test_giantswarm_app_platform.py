97e3db080f1b3bed90272e4abd41df914be18a58
Łukasz Piątkowski <piontec@gmail.com>
2020-06-23 08:55:46 +0200

    examples for app catalog

--- examples/test_giantswarm_app_platform.py
+++ examples/test_giantswarm_app_platform.py
@@ -1,14 +1,26 @@
-import pykube
+import logging
+
+import pytest
+from pykube.exceptions import HTTPError
 
 from pytest_helm_charts.apps.app_catalog import AppCatalogFactoryFunc
-from pytest_helm_charts.fixtures import Cluster
-import logging
 
 logger = logging.getLogger(__name__)
 
 
-def test_app_catalog_add_remove(kube_cluster: Cluster, app_catalog_factory: AppCatalogFactoryFunc):
+def test_app_catalog_add_remove(app_catalog_factory: AppCatalogFactoryFunc):
     catalog_name = "test-dynamic"
+    catalog_url = "https://test-dynamic.com/"
+    catalog = app_catalog_factory(catalog_name, catalog_url)
+    assert catalog.metadata["name"] == catalog_name
+    assert catalog.obj["spec"]["title"] == catalog_name
+    assert catalog.obj["spec"]["storage"]["type"] == "helm"
+    assert catalog.obj["spec"]["storage"]["URL"] == catalog_url
+
+
+@pytest.mark.xfail(raises=HTTPError)
+def test_app_catalog_bad_name(app_catalog_factory: AppCatalogFactoryFunc):
+    catalog_name = "test_dynamic"
     catalog_url = "https://test-dynamic.com/"
     catalog = app_catalog_factory(catalog_name, catalog_url)
     assert catalog.metadata["name"] == catalog_name
