066fbcee3c35a8ffdf74bbc85c1ab7a10ca3c132
Łukasz Piątkowski <piontec@gmail.com>
2020-07-02 16:31:36 +0200

    fixes for flake8

--- tests/test_example_app_platform.py
+++ tests/test_example_app_platform.py
@@ -6,8 +6,8 @@ from tests.helper import run_pytest
 
 def test_app_catalog_working_example(testdir: Testdir, mocker: MockFixture):
     testdir.copy_example("examples/test_giantswarm_app_platform.py")
-    catalog_name = "test-dynamic"
-    catalog_url = "https://test-dynamic.com"
+    # catalog_name = "test-dynamic"
+    # catalog_url = "https://test-dynamic.com"
 
     mocker.patch("pytest_helm_charts.giantswarm_app_platform.app_catalog.AppCatalogCR.create")
     # run pytest with the following cmd args
