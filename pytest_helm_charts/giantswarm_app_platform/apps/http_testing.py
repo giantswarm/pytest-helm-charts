603231b03553bfe33e4d09b596520ed0126137c4
Łukasz Piątkowski <piontec@gmail.com>
2020-07-02 10:03:36 +0200

    add gatling parser and wait_for_job

--- pytest_helm_charts/giantswarm_app_platform/apps/http_testing.py
+++ pytest_helm_charts/giantswarm_app_platform/apps/http_testing.py
@@ -1,11 +1,12 @@
 """This modules contains apps useful for testing HTTP applications."""
-from typing import Callable, Dict, Iterable, List, Optional
+import math
+from typing import Callable, Dict, Iterable, List, Optional, Tuple
 
 import pytest
 from pykube import HTTPClient, ConfigMap
 
-from pytest_helm_charts.giantswarm_app_platform.custom_resources import AppCR
 from pytest_helm_charts.giantswarm_app_platform.app import AppFactoryFunc
+from pytest_helm_charts.giantswarm_app_platform.custom_resources import AppCR
 from pytest_helm_charts.utils import YamlDict
 
 StormforgerLoadAppFactoryFunc = Callable[[int, str, Optional[Dict[str, str]]], AppCR]
@@ -27,6 +28,7 @@ def stormforger_load_app_factory(app_factory: AppFactoryFunc) -> StormforgerLoad
 
         >>> stormforger_load_app_factory(8, "loadtest.local", {"kubernetes.io/hostname": "localhost"})
     """
+
     def _stormforger_load_app_factory(replicas: int, host_url: str,
                                       node_affinity_selector: Optional[Dict[str, str]] = None) -> AppCR:
         """Creates and deploys stormforger load app by creating the relevant App CR in the API.
@@ -127,3 +129,98 @@ def gatling_app_factory(kube_client: HTTPClient,
 
     for cm in created_configmaps:
         cm.delete()
+
+
+class GatlingParser:
+    def __init__(self, lines: str) -> None:
+        split_lines = lines.splitlines()
+        start_line_query = [i for i in range(
+            len(split_lines)) if split_lines[i] == "Generating reports..."]
+        assert len(start_line_query) == 1
+        start_line = start_line_query[0]
+        report_lines = split_lines[start_line + 2:start_line + 20]
+        assert report_lines[0] == "================================================================================"
+        assert report_lines[1] == "---- Global Information --------------------------------------------------------"
+
+        total, ok, bad = self.__parse_result_line(
+            report_lines[2], "request count")
+        self.request_count_total, self.request_count_ok, self.request_count_bad = int(
+            total), int(ok), int(bad)
+        self.request_success_ratio = float(
+            self.request_count_ok) / float(self.request_count_total)
+        self.request_failure = float(
+            self.request_count_bad) / float(self.request_count_total)
+
+        min_time, _, _ = self.__parse_result_line(
+            report_lines[3], "min response time")
+        self.min_response_time = int(min_time)
+
+        max_time, _, _ = self.__parse_result_line(
+            report_lines[4], "max response time")
+        self.max_response_time = int(max_time)
+
+        mean_time, _, _ = self.__parse_result_line(
+            report_lines[5], "mean response time")
+        self.mean_response_time = int(mean_time)
+
+        std_deviation, _, _ = self.__parse_result_line(
+            report_lines[6], "std deviation")
+        self.std_deviation = int(std_deviation)
+
+        response_time_50th_percentile, _, _ = self.__parse_result_line(
+            report_lines[7], "response time 50th percentile")
+        self.response_time_50th_percentile = int(response_time_50th_percentile)
+
+        response_time_75th_percentile, _, _ = self.__parse_result_line(
+            report_lines[8], "response time 75th percentile")
+        self.response_time_75th_percentile = int(response_time_75th_percentile)
+
+        response_time_95th_percentile, _, _ = self.__parse_result_line(
+            report_lines[9], "response time 95th percentile")
+        self.response_time_95th_percentile = int(response_time_95th_percentile)
+
+        response_time_99th_percentile, _, _ = self.__parse_result_line(
+            report_lines[10], "response time 99th percentile")
+        self.response_time_99th_percentile = int(response_time_99th_percentile)
+
+        mean_rps, _, _ = self.__parse_result_line(report_lines[11], "mean requests/sec")
+        self.mean_rps = float(mean_rps)
+
+        assert report_lines[12] == "---- Response Time Distribution ------------------------------------------------"
+        self.response_time_distribution: Dict[Tuple[float, float], int] = {
+        }
+
+        line = report_lines[13][2:]
+        fields = list(filter(None, line.split(" ")))
+        assert fields[0] == "t"
+        upper = float(fields[2])
+        self.response_time_distribution[(0, upper)] = int(fields[4])
+
+        line = report_lines[14][2:]
+        fields = list(filter(None, line.split(" ")))
+        assert fields[3] == "t"
+        lower = float(fields[0])
+        upper = float(fields[5])
+        self.response_time_distribution[(lower, upper)] = int(fields[7])
+
+        line = report_lines[15][2:]
+        fields = list(filter(None, line.split(" ")))
+        assert fields[0] == "t"
+        lower = float(fields[2])
+        self.response_time_distribution[(lower, math.inf)] = int(fields[4])
+
+        line = report_lines[16][2:]
+        fields = list(filter(None, line.split(" ")))
+        assert fields[0] == "failed"
+
+    @staticmethod
+    def __parse_result_line(line, header: str) -> Tuple[str, str, str]:
+        assert line[0:2] == "> "
+        line = line[2:]
+        assert line.find(header) >= 0
+        fields = line[len(header):].split(" ")
+        fields = list(filter(None, fields))
+        total = fields[0]
+        ok = fields[1].split("=")[1]
+        bad = fields[2].split("=")[1].rstrip(")")
+        return total, ok, bad
--- pytest_helm_charts/utils.py
+++ pytest_helm_charts/utils.py
@@ -1,6 +1,34 @@
 """Different utilities required over the whole testing lib."""
+import logging
+import time
 from typing import Dict, Any
 
+import pykube.exceptions
+from pykube import HTTPClient, Job
 
 # YamlValue = Union[int, float, str, bool, List['YamlValue'], 'YamlDict']
+
 YamlDict = Dict[str, Any]
+
+logger = logging.getLogger(__name__)
+
+
+def wait_for_job(kube_client: HTTPClient, job_name: str, namespace: str, max_wait_time_sec: int):
+    job: Job
+    wait_time_sec = 0
+    while True:
+        try:
+            job = Job.objects(kube_client).filter(namespace=namespace).get(name=job_name)
+            status = job.obj["status"]
+            if status and "conditions" in status and len(status["conditions"]) and \
+                    status["conditions"][0]["type"] == "Complete":
+                break
+        except pykube.exceptions.ObjectDoesNotExist:
+            pass
+        if wait_time_sec >= max_wait_time_sec:
+            raise TimeoutError(
+                "Job {} in namespace {} was not completed in {} seconds".format(job_name, namespace, max_wait_time_sec))
+        logger.info("Waiting for gatling job to complete...")
+        time.sleep(1)
+        wait_time_sec += 1
+    return job
