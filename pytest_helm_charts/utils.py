76d78ee74f33ce80dc3b8eb89be10c2313196574
Łukasz Piątkowski <piontec@gmail.com>
2020-10-29 13:53:19 +0100

    more utils functions added

--- pytest_helm_charts/utils.py
+++ pytest_helm_charts/utils.py
@@ -102,6 +102,99 @@ def wait_for_deployments_to_run(
     return result
 
 
+def _statefulset_ready(s: pykube.StatefulSet) -> bool:
+    complete = "readyReplicas" in s.obj["status"] and s.replicas == int(s.obj["status"]["readyReplicas"])
+    return complete
+
+
+def wait_for_stateful_sets_to_run(
+    kube_client: HTTPClient,
+    stateful_set_names: List[str],
+    stateful_sets_namespace: str,
+    timeout_sec: int,
+    missing_ok: bool = False,
+) -> List[pykube.StatefulSet]:
+    result = wait_for_namespaced_objects_condition(
+        kube_client,
+        pykube.StatefulSet,
+        stateful_set_names,
+        stateful_sets_namespace,
+        _statefulset_ready,
+        timeout_sec,
+        missing_ok=missing_ok,
+    )
+    return result
+
+
+def ensure_namespace_exists(kube_client: pykube.HTTPClient, namespace_name: str) -> pykube.Namespace:
+    ns = pykube.Namespace.objects(kube_client).get_or_none(name=namespace_name)
+    if ns is None:
+        obj = {
+            "apiVersion": "v1",
+            "kind": "Namespace",
+            "metadata": {
+                "name": namespace_name,
+            },
+        }
+        ns = pykube.Namespace(kube_client, obj)
+        ns.create()
+    return ns
+
+
+def make_job_object(
+    kube_client: pykube.HTTPClient,
+    name_prefix: str,
+    namespace: str,
+    command: List[str],
+    image: str = "quay.io/giantswarm/busybox:1.32.0",
+    restart_policy: str = "OnFailure",
+    backoff_limit: int = 6,  # 6 is the default from k8s
+) -> pykube.Job:
+    return pykube.Job(
+        kube_client,
+        {
+            "apiVersion": "batch/v1",
+            "kind": "Job",
+            "metadata": {"generateName": name_prefix, "namespace": namespace},
+            "spec": {
+                "backoffLimit": backoff_limit,
+                "template": {
+                    "spec": {
+                        "containers": [
+                            {
+                                "name": f"{name_prefix}job",
+                                "image": image,
+                                "command": command,
+                            }
+                        ],
+                        "restartPolicy": restart_policy,
+                    },
+                },
+            },
+        },
+    )
+
+
+def create_job_and_run_to_completion(
+    kube_client: pykube.HTTPClient,
+    namespace: str,
+    job: pykube.Job,
+    timeout_sec: int = 60,
+    missing_ok: bool = False,
+) -> pykube.Job:
+    job.create()
+
+    wait_for_jobs_to_complete(
+        kube_client,
+        [job.name],
+        namespace,
+        timeout_sec,
+        missing_ok=missing_ok,
+    )
+
+    return job
+
+
 def proxy_http_request(client: HTTPClient, srv: Service, method, path, **kwargs) -> Response:
     """Template request to proxy of a Service.
     Args:
