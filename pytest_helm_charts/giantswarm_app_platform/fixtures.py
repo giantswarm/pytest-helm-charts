f0d2876dcfc1bb0761f8dcf77b98b8ff5cdb6a73
Łukasz Piątkowski <piontec@gmail.com>
2021-07-30 12:18:23 +0200

    add namespace_factory

--- pytest_helm_charts/giantswarm_app_platform/fixtures.py
+++ pytest_helm_charts/giantswarm_app_platform/fixtures.py
@@ -4,7 +4,6 @@ import pytest
 
 from .app import ConfiguredApp, AppFactoryFunc, app_factory_func
 from .app_catalog import AppCatalogFactoryFunc, AppCatalogCR, app_catalog_factory_func
-from .custom_resources import AppCR
 from ..clusters import Cluster
 
 
@@ -36,32 +35,10 @@ def app_factory(kube_cluster: Cluster, app_catalog_factory: AppCatalogFactoryFun
         # TODO: wait until finalizer is gone
 
 
-@pytest.fixture(scope="module")
-def kube_cluster_with_app_platform(
-    kube_cluster: Cluster, app_catalog_factory: AppCatalogFactoryFunc
-) -> Iterable[Cluster]:
-    """Get a ready cluster based on '--cluster-type' command line argument. Additionally,
-    preconfigure the cluster with Giant Swarm's Application Platform, including:
-    - app-operator
-    - chart-operator
-    - chartmuseum (for storing custom build time charts)
-    - AppCatalog Custom Resource configured for the chartmuseum."""
-    # FIXME: implement
-    # TODO:
-    # - deploy app-operator
-    # - deploy chartmuseum
-    # - create new AppCatalog CR with app_catalog_factory to register chartmuseum as catalog
-    raise NotImplementedError
-    # yield kube_cluster
-    # TODO:
-    # - destroy app-operator
-    # - destroy chartmuseum
-
-
-@pytest.fixture(scope="module")
-def my_chart() -> AppCR:
-    """Returns AppCR that can be used to deploy the chart under test using the App Platform
-    tools. The App resource is not yet deployed to the cluster. You need to call create()
-    and delete() to manage its deployment"""
-    # FIXME: implement
-    raise NotImplementedError
+# @pytest.fixture(scope="module")
+# def my_chart() -> AppCR:
+#     """Returns AppCR that can be used to deploy the chart under test using the App Platform
+#     tools. The App resource is not yet deployed to the cluster. You need to call create()
+#     and delete() to manage its deployment"""
+#     # FIXME: implement
+#     raise NotImplementedError
