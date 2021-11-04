63b3db6b005a9a78e64b94f640e29292a3029929
Łukasz Piątkowski <piontec@gmail.com>
2021-11-04 13:25:11 +0100

    add global wait_for_condition function as well

--- pytest_helm_charts/utils.py
+++ pytest_helm_charts/utils.py
@@ -15,6 +15,78 @@ logger = logging.getLogger(__name__)
 T = TypeVar("T", bound=pykube.objects.NamespacedAPIObject)
 
 
+def wait_for_objects_condition(
+    kube_client: HTTPClient,
+    obj_type: Type[T],
+    obj_names: List[str],
+    objs_namespace: str,
+    is_global: bool,
+    obj_condition_func: Callable[[T], bool],
+    timeout_sec: int,
+    missing_ok: bool,
+) -> List[T]:
+    """
+    Block until all the kubernetes objects of type `obj_type` pass `obj_condition_fun` or timeout is reached.
+
+    Args:
+        kube_client: client to use to connect to the k8s cluster
+        obj_type: type of the objects to check; they're usually derived from
+            [APIObject](pykube.objects.APIObject)
+        obj_names: a list of object resource names to check; all of the objects must be pass `obj_condition_fun`
+            for this function to succeed
+        objs_namespace: namespace where all the resources are created, if they are Namespace-scoped
+            (single namespace for all resources)
+        is_global: if True, the object is expected to be Cluster-scoped, if False then Namespace-scoped and the
+            objs_namespace above must be a valid namespace.
+        timeout_sec: timeout for the call
+        obj_condition_func: a function that gets one instance of the resource object of type `obj_type`
+            and returns boolean showing whether the object meets the condition or not. The call succeeds
+            only if this `obj_condition_fun` returns `True` for every object passed in `obj_names`.
+        missing_ok: when `True`, the function ignores that some of the objects listed in the `obj_names`
+            don't exist in k8s API and waits for them to show up; when `False`, an
+            [ObjectNotFound](pykube.exceptions.ObjectDoesNotExist) exception is raised.
+
+    Returns:
+        The list of namespaced object resources with all the objects listed in `obj_names` included.
+
+    Raises:
+        TimeoutError: when timeout is reached.
+        pykube.exceptions.ObjectDoesNotExist: when `missing_ok == False` and one of the objects
+            listed in `obj_names` can't be found in k8s API
+
+    """
+    if len(obj_names) == 0:
+        raise ValueError("'obj_names' list can't be empty.")
+
+    retries = 0
+    all_ready = False
+    matching_objs: List[T] = []
+    while retries < timeout_sec:
+        response = obj_type.objects(kube_client)
+        if not is_global:
+            response = response.filter(namespace=objs_namespace)
+        retries += 1
+        matching_objs = []
+        for name in obj_names:
+            try:
+                obj = response.get_by_name(name)
+                matching_objs.append(obj)
+            except pykube.exceptions.ObjectDoesNotExist:
+                if missing_ok:
+                    pass
+                else:
+                    raise
+        all_ready = len(matching_objs) == len(obj_names) and all(obj_condition_func(obj) for obj in matching_objs)
+        if all_ready:
+            break
+        time.sleep(1)
+
+    if not all_ready:
+        raise TimeoutError(f"Error waiting for object of type {obj_type} to match the condition.")
+
+    return matching_objs
+
+
 def wait_for_namespaced_objects_condition(
     kube_client: HTTPClient,
     obj_type: Type[T],
@@ -51,34 +123,48 @@ def wait_for_namespaced_objects_condition(
             listed in `obj_names` can't be found in k8s API
 
     """
-    if len(obj_names) == 0:
-        raise ValueError("'obj_names' list can't be empty.")
+    return wait_for_objects_condition(
+        kube_client, obj_type, obj_names, objs_namespace, False, obj_condition_func, timeout_sec, missing_ok
+    )
 
-    retries = 0
-    all_ready = False
-    matching_objs: List[T] = []
-    while retries < timeout_sec:
-        response = obj_type.objects(kube_client).filter(namespace=objs_namespace)
-        retries += 1
-        matching_objs = []
-        for name in obj_names:
-            try:
-                obj = response.get_by_name(name)
-                matching_objs.append(obj)
-            except pykube.exceptions.ObjectDoesNotExist:
-                if missing_ok:
-                    pass
-                else:
-                    raise
-        all_ready = len(matching_objs) == len(obj_names) and all(obj_condition_func(obj) for obj in matching_objs)
-        if all_ready:
-            break
-        time.sleep(1)
 
-    if not all_ready:
-        raise TimeoutError(f"Error waiting for object of type {obj_type} to match the condition.")
+def wait_for_global_objects_condition(
+    kube_client: HTTPClient,
+    obj_type: Type[T],
+    obj_names: List[str],
+    obj_condition_func: Callable[[T], bool],
+    timeout_sec: int,
+    missing_ok: bool,
+) -> List[T]:
+    """
+    Block until all the namespaced kubernetes objects of type `obj_type` pass `obj_condition_fun` or timeout is reached.
 
-    return matching_objs
+    Args:
+        kube_client: client to use to connect to the k8s cluster
+        obj_type: type of the objects to check; they're mostly derived from
+            [APIObject](pykube.objects.APIObject)
+        obj_names: a list of object resource names to check; all of the objects must be pass `obj_condition_fun`
+            for this function to succeed
+        timeout_sec: timeout for the call
+        obj_condition_func: a function that gets one instance of the resource object of type `obj_type`
+            and returns boolean showing whether the object meets the condition or not. The call succeeds
+            only if this `obj_condition_fun` returns `True` for every object passed in `obj_names`.
+        missing_ok: when `True`, the function ignores that some of the objects listed in the `obj_names`
+            don't exist in k8s API and waits for them to show up; when `False`, an
+            [ObjectNotFound](pykube.exceptions.ObjectDoesNotExist) exception is raised.
+
+    Returns:
+        The list of namespaced object resources with all the objects listed in `obj_names` included.
+
+    Raises:
+        TimeoutError: when timeout is reached.
+        pykube.exceptions.ObjectDoesNotExist: when `missing_ok == False` and one of the objects
+            listed in `obj_names` can't be found in k8s API
+
+    """
+    return wait_for_objects_condition(
+        kube_client, obj_type, obj_names, "", True, obj_condition_func, timeout_sec, missing_ok
+    )
 
 
 def _job_complete(job: Job) -> bool:
