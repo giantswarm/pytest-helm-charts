9aa7bd933787d4d9c9b2419a4a9032d5d002976b
Łukasz Piątkowski <piontec@gmail.com>
2021-12-09 17:46:33 +0100

    Job API moved

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,8 @@ from pykube import HTTPClient
 from pykube.objects import NamespacedAPIObject
 from pytest_mock import MockerFixture, MockFixture
 
-from pytest_helm_charts.utils import wait_for_namespaced_objects_condition, make_job_object
+from pytest_helm_charts.utils import wait_for_namespaced_objects_condition
+from pytest_helm_charts.api.job import make_job_object
 
 MockCR = NamespacedAPIObject
 
