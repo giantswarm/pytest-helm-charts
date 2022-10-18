9b0371b147a9544b233ebe80f13ca0b66c001188
Łukasz Piątkowski <piontec@gmail.com>
2022-10-18 16:49:22 +0200

    fix: cmd line parameters take the highest priority, so they can't have defaults

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,3 +1,6 @@
+import os
+
+import pytest
 from _pytest.fixtures import FixtureRequest
 from pytest_mock import MockFixture
 
@@ -6,3 +9,16 @@ def test_temp_namespace(request: FixtureRequest, mocker: MockFixture) -> None:
     ns_fixture = request.getfixturevalue("random_namespace")
     assert ns_fixture is not None
     ns_fixture.delete = mocker.MagicMock()
+
+
+def test_kubeconfig_path_env_var(request: FixtureRequest) -> None:
+    test_path = "env/kube.config"
+    os.environ["KUBECONFIG"] = test_path
+    kubeconfig_path = request.getfixturevalue("kube_config")
+    assert kubeconfig_path == test_path
+
+
+def test_kubeconfig_path_no_val(request: FixtureRequest) -> None:
+    with pytest.raises(Exception) as e:
+        request.getfixturevalue("kube_config")
+        assert e.__str__().startswith("Environment variable 'KUBECONFIG'")
